22ff7b87c5879eacda183e5f63f8e51a6b86af94
Ryan Prince <willbluemoon99@gmail.com>
2025-03-27 17:54:45 +0700

    Addnew controller for the 3rd screen

---
+++ A2_iOS_Ryan_101460443_1/AddProductViewController.swift
@@ -0,0 +1,29 @@
+//
+//  AddProductViewController.swift
+//  A2_iOS_Ryan_101460443_1
+//
+//  Created by Christian Aiden on 2025-03-27.
+//
+
+import UIKit
+
+class AddProductViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
