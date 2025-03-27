871bc963405de33514e69f017e0d180a478160c9
Ryan Prince <willbluemoon99@gmail.com>
2025-03-27 15:58:35 +0700

    Add UI for the first screen

--- A2_iOS_Ryan_101460443_1/ViewController.swift
+++ A2_iOS_Ryan_101460443_1/ViewController.swift
@@ -9,6 +9,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var searchBar: UISearchBar!
+    //labels to display the first product
+    @IBOutlet weak var providerLabel: UILabel!
+    @IBOutlet weak var priceLabel: UILabel!
+    @IBOutlet weak var descriptionLabel: UILabel!
+    @IBOutlet weak var productNameLabel: UILabel!
+    @IBOutlet weak var productIdLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
