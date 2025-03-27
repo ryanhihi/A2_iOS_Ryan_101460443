fdfa861d7a4144be43b34b81c78c231726620a64
Ryan Prince <willbluemoon99@gmail.com>
2025-03-27 18:34:04 +0700

    add saveNewProduct fucntion

--- A2_iOS_Ryan_101460443_1/AddProductViewController.swift
+++ A2_iOS_Ryan_101460443_1/AddProductViewController.swift
@@ -35,7 +35,34 @@ class AddProductViewController: UIViewController {
     */
     
     
-    @IBAction func saveNewProduct(_ sender: Any) {
+    @IBAction func saveNewProduct(_ sender: UIButton) {
+        
+        guard let name = textFields[0].text, !name.isEmpty,
+            let description = textFields[1].text, !description.isEmpty,
+            let priceText = textFields[2].text, let price = Double(priceText),
+              let provider = textFields[3].text, !provider.isEmpty else {
+            showAlert("Error", "Please fill in all fields correctly.")
+            return
+        }
+        
+        // Create a new Product
+                let newProduct = Product(context: context)
+                newProduct.productID = UUID()
+                newProduct.name = name
+                newProduct.desc = description
+                newProduct.price = price
+                newProduct.provider = provider
+        
+        do {
+                    try context.save()
+                    showAlert("Success", "Product saved successfully.") { [weak self] in
+                        self?.clearFields()
+                    }
+                } catch {
+                    showAlert("Error", "Failed to save product: \(error.localizedDescription)")
+                }
     }
+    
+    
     
 }
